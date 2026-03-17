4f6e7597afe9aa39eb54475fed8bfe2dd0dc36ba
hirmy630-droid <hirmy630@gmail.com>
2026-03-17 10:48:43 +0900

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,23 @@
+# チーズ管計算 PWA
+
+GitHub Pages にそのまま置ける構成です。
+
+## 配置するファイル
+- index.html
+- manifest.json
+- sw.js
+- icon-180.png
+- icon-192.png
+- icon-512.png
+- icon-1024.png
+- splash-1170x2532.png
+
+## 公開手順
+1. GitHub で新規リポジトリを作成
+2. このファイル一式を root にアップロード
+3. Settings > Pages > Deploy from branch > main / root
+4. 公開URLを開く
+
+## インストール
+- Android: 画面右下の「アプリをインストール」
+- iPhone/iPad: Safari の共有 > ホーム画面に追加
---
+++ sw.js
@@ -0,0 +1,59 @@
+
+const CACHE_VERSION = 'cheese-native-grade-v1';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-180.png',
+  './icon-192.png',
+  './icon-512.png',
+  './icon-1024.png',
+  './splash-1170x2532.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_VERSION).then((cache) => cache.addAll(APP_SHELL)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(
+      keys.map((key) => key !== CACHE_VERSION ? caches.delete(key) : Promise.resolve())
+    )).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
+  const url = new URL(event.request.url);
+
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          const copy = response.clone();
+          caches.open(CACHE_VERSION).then((cache) => cache.put('./index.html', copy));
+          return response;
+        })
+        .catch(() => caches.match('./index.html'))
+    );
+    return;
+  }
+
+  if (url.origin === location.origin) {
+    event.respondWith(
+      caches.match(event.request).then((cached) => {
+        if (cached) return cached;
+        return fetch(event.request).then((response) => {
+          if (!response || response.status !== 200) return response;
+          const copy = response.clone();
+          caches.open(CACHE_VERSION).then((cache) => cache.put(event.request, copy));
+          return response;
+        });
+      })
+    );
+  }
+});
