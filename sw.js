c631117035d661ed0293681de032145077234060
hirmy630-droid <hirmy630@gmail.com>
2026-03-26 18:28:45 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,21 +1,32 @@
+const CACHE_NAME = 'cheese-v2';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-180.png',
+  './icon-192.png',
+  './icon-512.png'
+];
 
-const CACHE_VERSION = 'design-restored-v1';
 self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE_VERSION).then((cache) => cache.addAll(['./','./index.html','./manifest.json','./icon-180.png','./icon-192.png','./icon-512.png','./icon-1024.png'])).then(() => self.skipWaiting()));
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
 });
+
 self.addEventListener('activate', (event) => {
-  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.map((key) => key !== CACHE_VERSION ? caches.delete(key) : Promise.resolve()))).then(() => self.clients.claim()));
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((key) => key !== CACHE_NAME ? caches.delete(key) : Promise.resolve()))
+    ).then(() => self.clients.claim())
+  );
 });
+
 self.addEventListener('fetch', (event) => {
   if (event.request.method !== 'GET') return;
   event.respondWith(
-    caches.match(event.request).then((cached) => {
-      if (cached) return cached;
-      return fetch(event.request).then((response) => {
-        const copy = response.clone();
-        caches.open(CACHE_VERSION).then((cache) => cache.put(event.request, copy));
-        return response;
-      }).catch(() => caches.match('./index.html'));
-    })
+    caches.match(event.request).then((res) => res || fetch(event.request))
   );
 });
